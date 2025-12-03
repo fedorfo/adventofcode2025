73debc1613ac26b21938524770b3814af1c10e5c
fedor.fominyh <fedor@anna.money>
2025-12-03 14:48:03 +0000

    Day #03

---
+++ AdventOfCode2025/Puzzles/Day03.cs
@@ -0,0 +1,24 @@
+namespace AdventOfCode2025.Puzzles;
+
+public class Day03 : PuzzleBase
+{
+    private long MaxJoltage(string bank, int len)
+    {
+        var dp = new long[bank.Length + 1, len + 1];
+        for (var i = 1; i <= bank.Length; i++)
+        {
+            var d = bank[i - 1] - '0';
+            for (var j = 1; j <= len; j++)
+                for (var k = 0; k < i; k++)
+                    dp[i, j] = Math.Max(dp[i, j], dp[k, j - 1] * 10 + d);
+        }
+        return Enumerable.Range(0, bank.Length + 1).Max(i => dp[i, len]);
+    }
+
+    public override void Solve()
+    {
+        var banks = ReadBlockLines().ToList();
+        Console.WriteLine(banks.Sum(b => MaxJoltage(b, 2)));
+        Console.WriteLine(banks.Sum(b => MaxJoltage(b, 12)));
+    }
+}
